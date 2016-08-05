5c6cc3f0493fd2da4242200f1e5f7dd4a109a609
Tanmay <tanmaykacker40@gmail.com>
2016-08-06 00:11:13 +0530

    First Commit
    Included
    - Package JSON
    - Model

---
+++ app/models/todo.model.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var toDoSchema = schema({
+	todo : {
+		type : String
+	},
+
+	completed : {
+		type : Boolean
+	},
+
+	created_on : {
+		type : Date,
+		default : Date.now()
+	}
+
+	finished_by : {
+		type : Date
+	}
+});
+
+var toDoModel = mongoose.model('Todo', toDoSchema);
+module.exports = toDoSchema;
